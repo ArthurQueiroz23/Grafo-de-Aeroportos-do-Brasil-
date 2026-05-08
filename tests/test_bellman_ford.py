cf3176adae13e0e79c630fb63cb7566c4c3e2bf7
antoniolaprov <aaal@cesar.school>
2026-05-08 15:40:32 -0300

    testes: adicionando o  caso de belman ford com peso negativo sem ciclo negativo

--- tests/test_bellman_ford.py
+++ tests/test_bellman_ford.py
@@ -8,9 +8,23 @@ sys.path.insert(0, str(ROOT))
 
 from src.graphs.algorithms import bellman_ford, shortest_path
 from src.graphs.graph import Graph
+from src.graphs.digraph import DiGraph
+from src.graphs.directed_algorithms import bellman_ford_digraph
+
 
 
 class TestBellmanFord(unittest.TestCase):
+    
+    def test_pesoNegativoSemCiclo(self ) -> None:
+        g = DiGraph()
+
+        g.add_edge(0, 1, 2.0)
+        g.add_edge(1, 2, -3.0)
+        g.add_edge(0, 2, 100.0)
+        dist_bf, _, neg = bellman_ford_digraph(g, 0)
+        self.assertFalse(neg)
+        self.assertAlmostEqual(dist_bf[2], -1.0)
+
 
     def test_matches_dijkstra_when_nonneg(self) -> None:
         g = Graph()
